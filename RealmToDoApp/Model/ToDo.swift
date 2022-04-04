1be577534e6604cd8f68bc6caad4b70450348bff
Jager-yoo <y73447jh@gmail.com>
2022-04-05 02:50:33 +0900

    feat: ToDo 모델 타입 구현

---
+++ RealmToDoApp/Model/ToDo.swift
@@ -0,0 +1,39 @@
+//
+//  ToDo.swift
+//  RealmToDoApp
+//
+//  Created by 유재호 on 2022/04/05.
+//
+
+import Foundation
+import RealmSwift
+
+final class ToDo: Object, ObjectKeyIdentifiable {
+    
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var name: String
+    @Persisted var completed: Bool = false
+    @Persisted var urgency: Urgency = .neutral
+    
+    enum Urgency: Int, PersistableEnum {
+        
+        case trivial
+        case neutral
+        case urgent
+        
+        var text: String {
+            switch self {
+            case .trivial:
+                return "Trivial"
+            case .neutral:
+                return "Neutral"
+            case .urgent:
+                return "Urgent"
+            }
+        }
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+}
