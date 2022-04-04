b465a0fbe202d86c1e0daa68d2669878a2501bc3
Jager-yoo <y73447jh@gmail.com>
2022-04-05 02:10:54 +0900

    Initial Commit

---
+++ RealmToDoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmToDoApp
+//
+//  Created by 유재호 on 2022/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealmToDoApp/RealmToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmToDoAppApp.swift
+//  RealmToDoApp
+//
+//  Created by 유재호 on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct RealmToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
