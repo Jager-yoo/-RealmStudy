cd7bf34159cde6842825ea63a02aaf1fad4efce9
Jager-yoo <y73447jh@gmail.com>
2022-04-05 03:44:35 +0900

    chore: Realm 데이터 저장 위치를 확인하기 위한 print문 삽입

--- RealmToDoApp/RealmToDoAppApp.swift
+++ RealmToDoApp/RealmToDoAppApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct RealmToDoAppApp: App {
+    // App 프로토콜? -> https://developer.apple.com/documentation/swiftui/app
+    
     var body: some Scene {
         WindowGroup {
             ToDoListView()
+                .onAppear {
+                    // Realm 데이터가 어디에 저장되는지 확인하기 위한 코드
+                    print("⚠️⚠️⚠️")
+                    print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path)
+                    
+                    // 콘솔로그에 지저분하게 많이 뜨는 레이아웃 관련 오류를 안 뜨게 해주는 코드
+                    UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+                }
         }
     }
 }
