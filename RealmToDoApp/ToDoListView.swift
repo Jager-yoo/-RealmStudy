b4911df3d2a32f3125994a2c7236ee3125aa1d75
Jager-yoo <y73447jh@gmail.com>
2022-04-05 03:17:10 +0900

    feat: TextField 입력 후 추가 버튼을 누르면 키보드가 내려가는 기능 추가
    - iOS 15.0+ 부터 사용 가능한 FocusState 프로퍼티 wrapper 적용

--- RealmToDoApp/ToDoListView.swift
+++ RealmToDoApp/ToDoListView.swift
@@ -12,6 +12,7 @@ struct ToDoListView: View {
     
     @ObservedResults(ToDo.self) var ToDos
     @State private var name = ""
+    @FocusState private var focus: Bool?
     
     var body: some View {
         NavigationView {
@@ -19,16 +20,20 @@ struct ToDoListView: View {
                 HStack {
                     TextField("New ToDo", text: $name)
                         .textFieldStyle(.roundedBorder)
+                        .focused($focus, equals: true)
                     Spacer()
                     Button {
                         let newToDo = ToDo(name: name)
                         $ToDos.append(newToDo)
                         name = ""
+                        focus = nil // 버튼이 눌리면, focus 를 nil 로 만들어서, 키보드가 내려가도록 함
                     } label: {
                         Image(systemName: "plus.circle.fill")
                     }
                     .disabled(name.isEmpty)
                 }
+                .padding()
+                
                 List {
                     ForEach(ToDos) { toDo in
                         Text(toDo.name)
